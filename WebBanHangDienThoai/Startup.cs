49c1518d08adcd2afc3b431523bab40a8ce9db25
TrungHai020396 <hunghaihuy9@gmail.com>
2017-10-13 09:43:23 +0700

    Add project files.

---
+++ WebBanHangDienThoai/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBanHangDienThoai.Startup))]
+namespace WebBanHangDienThoai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

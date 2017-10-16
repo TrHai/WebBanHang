b7f912bc9c4bb0eedc778122b348ba46f7ed9713
TrungHai020396 <hunghaihuy9@gmail.com>
2017-10-16 15:07:27 +0700

    test2

--- WebBanHangDienThoai/Controllers/HomeController.cs
+++ WebBanHangDienThoai/Controllers/HomeController.cs
@@ -3,27 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHangDienThoai.Models;
 
 namespace WebBanHangDienThoai.Controllers
 {
     public class HomeController : Controller
     {
+        QLBanHangDienThoaiEntities db = new QLBanHangDienThoaiEntities();
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            var lstDTM = db.SanPhams.Where(n => n.MaLoaiSP == 1 && n.Moi == 1 && n.DaXoa==0);
+            ViewBag.ListDTM = lstDTM;
             return View();
         }
     }
---
+++ WebBanHangDienThoai/Controllers/SanPhamController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangDienThoai.Models;
+
+namespace WebBanHangDienThoai.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        QLBanHangDienThoaiEntities db = new QLBanHangDienThoaiEntities();
+        // GET: SanPham
+        [ChildActionOnly]//để chặn người dùng ko thể Get vào partial này
+        public ActionResult SanPhamStyle1Partial()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]//để chặn người dùng ko thể Get vào partial này
+        public ActionResult SanPhamStyle2Partial()
+        {
+            return PartialView();
+        }
+        public ActionResult XemChiTiet(int? id)
+        {
+            
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadGateway);
+            }
+          
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id && n.DaXoa==0);
+            if(sp==null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+    }
+}
